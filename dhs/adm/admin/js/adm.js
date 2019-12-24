8566115c397a87c3784446ab343cc24722d2df33
dholahighschool <58439434+dholahighschool@users.noreply.github.com>
2019-12-24 16:42:04 +0530

    Dhola High School

---
+++ dhs/adm/admin/js/adm.js
@@ -0,0 +1,32 @@
+function toEdit(){
+var uid = document.getElementById("appid").value;
+    if (window.sessionStorage) 
+        {
+          sessionStorage.setItem("username", uid);
+          window.open("../student/edit.html");
+        }
+    }
+    function toixLogin(){
+var uid = document.getElementById("appid").value;
+    if (window.sessionStorage) 
+        {
+          sessionStorage.setItem("username", uid);
+          window.open("../student/login/ixonly.html");
+        }
+    }
+    function toviLogin(){
+var uid = document.getElementById("appid").value;
+    if (window.sessionStorage) 
+        {
+          sessionStorage.setItem("username", uid);
+          window.open("../student/login/vitoviii.html");
+        }
+    }
+    function admstupd(){
+var uid = document.getElementById("appid").value;
+    if (window.sessionStorage) 
+        {
+          sessionStorage.setItem("username", uid);
+          window.open("../student/admission.html");
+        }
+    }
