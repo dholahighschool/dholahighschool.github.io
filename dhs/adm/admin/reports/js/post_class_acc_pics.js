21a40dddf0d1e2234cae727b3254edef42ddcdc3
dholahighschool <58439434+dholahighschool@users.noreply.github.com>
2020-01-06 02:10:37 +0530

    Dhola High School

--- dhs/adm/admin/reports/js/post_class_acc_pics.js
+++ dhs/adm/admin/reports/js/post_class_acc_pics.js
@@ -50,11 +50,21 @@ var updaadhaarpic = snap.child("updaadhaarpic").val();
 var updbpass = snap.child("updbpass").val();
 var updcaste = snap.child("updcaste").val();
 var appid = snap.child("appid").val();
+var n = updphoto.indexOf("token=");
+var pht = updphoto.substring(n + 6, updphoto.length);
 
+var n = updsign.indexOf("token=");
+var sgn = updsign.substring(n + 6, updsign.length);
+
+var n = updbpass.indexOf("token=");
+var bps = updbpass.substring(n + 6, updbpass.length);
+
+var n = updbproof.indexOf("token=");
+var bpr = updbproof.substring(n + 6, updbproof.length);
 
 if(prclass==strm)
 {
-$("#table_body").append("<tr><td>" + appid + "</td><td>CLASS " + prclass + "</td><td>" + prsec + "</td><td>" + prroll + "</td><td><img src=\"" + updphoto + "\" height=100 width=100></img><br><a href=\"" + updphoto + "\">Download</a></td><td><img src=\"" + updsign + "\"  height=100 width=100></img></td><td><img src=\"" + updbproof + "\"  height=100 width=100></img></td><td><img src=\"" + updslc + "\"  height=100 width=100></img></td><td><img src=\"" + updaadhaarpic + "\"  height=100 width=100></img></td><td><img src=\"" + updbpass + "\"  height=100 width=100></img></td><td><img src=\"" + updcaste + "\"  height=100 width=100></img></td></tr>");
+$("#table_body").append("<tr><td>" + appid + "</td><td>CLASS " + prclass + "</td><td>" + prsec + "</td><td>" + prroll + "</td><td><img src=\"" + updphoto + "\" height=100 width=100></img><br><a href=\"" + updphoto + "\">Download</a></td><td><img src=\"" + updsign + "\"  height=100 width=100></img></td><td><img src=\"" + updbproof + "\"  height=100 width=100></img></td><td><img src=\"" + updslc + "\"  height=100 width=100></img></td><td><img src=\"" + updaadhaarpic + "\"  height=100 width=100></img></td><td><img src=\"" + updbpass + "\"  height=100 width=100></img></td><td><img src=\"" + updcaste + "\"  height=100 width=100></img></td><td><input type='submit' id='last' class='button' value='Download' name='line' onclick=\"addField(\'" + appid +"\',\'" + pht +"\',\'" + sgn +"\',\'" + bps +"\',\'" + bpr +"\');\"></td></tr>");
 }
 
 });
@@ -65,4 +75,27 @@ $("#table_body").append("<tr><td>" + appid + "</td><td>CLASS " + prclass + "</td
 function myFunction() {
   var rowCount = $('#table_body tr').length;
 $("#rowscnt").text("Total No. of Students = " + rowCount +"");
+}
+function addField(appid,updphoto,updsign,updbpass,updbproof){
+  
+  
+  var files = [];
+  files.push("https://firebasestorage.googleapis.com/v0/b/admission-2020.appspot.com/o/images%2F" + appid + "%2Fphoto?alt=media&token=" + updphoto);
+  files.push("https://firebasestorage.googleapis.com/v0/b/admission-2020.appspot.com/o/images%2F" + appid + "%2Fsign?alt=media&token=" + updsign);
+  files.push("https://firebasestorage.googleapis.com/v0/b/admission-2020.appspot.com/o/images%2F" + appid + "%2Fbpass?alt=media&token=" + updbpass);
+  files.push("https://firebasestorage.googleapis.com/v0/b/admission-2020.appspot.com/o/images%2F" + appid + "%2Fbproof?alt=media&token=" + updbproof);
+  for(var ii=0; ii<files.length; ii++){
+    window.open(files[ii],'_blank');
+  }
+  /*
+  
+    //window.open(lnk);
+  alert(lnk);
+
+var myWindow = window.open("");
+  myWindow.document.write("<title>Dhola High School</title><iframe width='100%' height='100%' src='" + lnk + "' frameborder='0' allowfullscreen></iframe>");
+
+var lnk = "https://firebasestorage.googleapis.com/v0/b/admission-2020.appspot.com/o/images%2F" + appid + "%2Fphoto?alt=media&token=" + updphoto;
+*/
+
 }
