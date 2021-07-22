fe9c6834688d8f07b53c23c5ed43d893b9aaf984
dholahighschool <58439434+dholahighschool@users.noreply.github.com>
2021-07-22 12:33:00 +0530

    Updated

--- dhs/adm/student/js/xioh.js
+++ dhs/adm/student/js/xioh.js
@@ -1297,7 +1297,7 @@ function arl() {
 rootRef.on("child_added", snap => {
 
 var cat = snap.child("prstream").val();
-  ars = 0;
+  ars = ars - 1;
   $('#seatlive').html("<center><b><i>Seats Available (LIVE): </i></b><br>Arts: <b>" + ars + "</b>&nbsp; Science: <b>" + scs + "</b>&nbsp; Commerce: <b>" + cms + "</b></center>");
   $('#showmore').attr('ars', ars);
 });
