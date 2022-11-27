e7e6a12ab787d42e7ef858ec7a363bc86a902a2d
dholahighschool <58439434+dholahighschool@users.noreply.github.com>
2022-11-27 19:14:30 +0530

    Updated

--- dhs/adm/student/js/vtoixregister.js
+++ dhs/adm/student/js/vtoixregister.js
@@ -50,7 +50,7 @@ function saveMessage(appid,name,dob,gname,prclass,schooltype,bsid,regID,meet){
   var newMessageRef = messagesRef.push();
   MessageRef.set({
   */
-  firebase.database().ref('vtoix2022/' + appid).update({
+  firebase.database().ref('vtoix2023/' + appid).update({
 appid:appid.toString(),
 name:name,
 dob:dob,
@@ -59,7 +59,7 @@ prclass:prclass,
 schooltype:schooltype,
 bsid:bsid,
 regID:regID,
-acyear:"2022",
+acyear:"2023",
 meet:meet
   });
 }
@@ -73,7 +73,7 @@ Swal.fire({
   allowOutsideClick:false,
   allowEscapeKey:false
 });
-var rootRef = firebase.database().ref('vtoix2022');
+var rootRef = firebase.database().ref('vtoix2023');
 rootRef.on("child_added", snap => {
 var appid = snap.child("appid").val();
 var regID = snap.child("regID").val();
