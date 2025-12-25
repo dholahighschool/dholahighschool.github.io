9eefeb94817a829519aaa308403773130635d3b0
dholahighschool <58439434+dholahighschool@users.noreply.github.com>
2025-12-25 09:34:23 +0530

    BR tag updated

--- dhs/adm/student/js/vtoixregister.js
+++ dhs/adm/student/js/vtoixregister.js
@@ -1,5 +1,5 @@
 document.getElementById('prclass').value = location.search.substring(1).split("=")[1].toUpperCase();
-var meetDate = location.search.substring(1).split("=")[2].toUpperCase().split("%20").join(" ").split("%3CBR%3E").join(" ");
+var meetDate = location.search.substring(1).split("=")[2].toUpperCase().split("%20").join(" ");
   if(location.search.substring(1).split("=")[3] == 'admin'){
     document.getElementById('prclass').disabled = false;
   }
@@ -150,6 +150,7 @@ else{
   function fillupform(){
     window.location.assign('student/admission.html?uid=' + timestamp);
   }
+
 
 
 
